be1063132ca6aaaa3b1fbf0ebb65362dba170e1e
Nafiz Kamal <nfzkml11.cs@gmail.com>
2025-02-19 17:12:53 +0600

    update 3. Storing Elements in a Collection

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.util.*;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 /**
  * 2025, January 22, Wednesday, 10:31 PM
@@ -10,5 +8,17 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) {
 
+        Collection<String> collection = new ArrayList<>();
+        collection.add("a");
+        collection.add("");
+        collection.add(null);
+        collection.add("b");
+        collection.add("");
+        collection.add(null);
+        collection.add("c");
+        System.out.println(collection);     // [a, , null, b, , null, c]
+
+        collection.removeIf(((Predicate<String>) Objects::isNull).or(String::isEmpty));
+        System.out.println(collection);     // [a, b, c]
     }
 }
