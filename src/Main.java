0a8c44effb674cc61722027925f206114b79b285
Nafiz Kamal <nfzkml11.cs@gmail.com>
2025-03-19 22:48:53 +0600

    update 1. Introducing Generics

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
-import com.sun.source.tree.Tree;
-
 import java.util.*;
-import java.util.function.Predicate;
 
 /**
- * 2025, January 22, Wednesday, 10:31 PM
+ * hi
  */
 
 public class Main {
     public static void main(String[] args) {
 
     }
+
+
 }
